6063b704377f923da70b98f2d3b8f49627c9500a
Lekshmana Perumal M <lekshmanan.sudali954@gmail.com>
2014-09-28 14:44:32 +0530

    Update Maxmind.java

--- File-Operation/MyProject/src/Maxmind.java
+++ File-Operation/MyProject/src/Maxmind.java
@@ -11,7 +11,7 @@ import com.maxmind.geoip2.model.CountryResponse;
 public class Maxmind {
 
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		final WebServiceClient serviceClient = new WebServiceClient.Builder(90704, "9s5FwsdDXCP0").build();
+		final WebServiceClient serviceClient = new WebServiceClient.Builder(90704, "YYYYYYYYY").build();
 
 		try
 		{
